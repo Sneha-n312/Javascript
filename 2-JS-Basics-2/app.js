d335d17dbc136c5044f18ba0ded15aadfdd33d2e
Sneha-n312 <55570335+Sneha-n312@users.noreply.github.com>
2021-07-08 13:47:55 +0530

    2. Javascript Statements, Helper methods

---
+++ 2-JS-Basics-2/app.js
@@ -0,0 +1,13 @@
+// Helper methods :
+//1. document.write('Hello World')
+//2. console.log('Hello World')
+//3. alert('Hello World')
+
+document.write('Hello World') // - writes in the browser page itself
+alert('Hello World') // - popup box
+console.log('Hello world') // - writes in the console
+
+document.write({ name: 'john' }) // will print {object Object}
+alert({ name: 'john' }) // - will print {object Object}
+console.log({ name: 'john' }) // - will print {name : john} - hence this is more powerful in debugging
+
