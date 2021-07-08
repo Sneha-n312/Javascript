ff76d9ea7a48e2e4c401babbdf83d8708681909d
Sneha-n312 <55570335+Sneha-n312@users.noreply.github.com>
2021-07-08 13:49:36 +0530

    4. Javascript Numbers and Arithmetic

---
+++ 4-JS-Basics-4/app.js
@@ -0,0 +1,36 @@
+//Numbers
+//Loosely Typed - Don't declare type
+
+const number = 39
+let pants = 4.53
+
+console.log(pants)
+pants = 'are great'
+console.log(pants);
+
+const number2 = 3.78
+const number3 = '22.667' // A string variable
+
+console.log(number3);
+
+const add = number + number2
+const sub = number - number2
+const mul = number * number2
+const div = number / number2
+
+console.log(number);
+console.log(number2);
+console.log(add);
+console.log(sub);
+console.log(mul);
+console.log(div);
+
+// +=, -=, *=, /=, ++, --, % All these operations can be performed on numbers
+
+const random = 6+4+10*10
+const random2 = (6+4+10)*10
+
+console.log(random);
+console.log(random2);
+
+
