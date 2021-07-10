71b6279f315a09c2f26af728982c4a0468c93c3c
Sneha-n312 <55570335+Sneha-n312@users.noreply.github.com>
2021-07-10 23:12:40 +0530

    10. JS Switch Statements

---
+++ 10-JS-Basics-10/app.js
@@ -0,0 +1,28 @@
+// Switch Statements
+
+// dice values: 1 - 6
+
+const dice = 05;
+
+switch(dice){
+    case 1: 
+        console.log('Dice value is 1');
+        break
+    case 2:
+         console.log('Dice value is 2');
+         break
+    case 3: 
+        console.log('Dice value is 3');
+        break
+    case 4: 
+        console.log('Dice value is 4');
+        break
+    case 5: 
+        console.log('Dice value is 5');
+        break
+    case 6: 
+        console.log('Dice value is 6');
+        break
+    default: 
+        console.log('You did not roll the dice');
+}
