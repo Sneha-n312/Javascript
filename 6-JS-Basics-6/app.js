c4ababbdb03aa01c4df10bea1269a422bc91446d
Sneha-n312 <55570335+Sneha-n312@users.noreply.github.com>
2021-07-10 23:09:54 +0530

    6. JS Datatypes

---
+++ 6-JS-Basics-6/app.js
@@ -0,0 +1,28 @@
+// Datatypes - Total 7
+// Primitive - String, Number, Boolean, Null, Undefined, Symbol
+//Object - Arrays, Functions, Objects
+
+//typeof - operator (type of variable) (type of value)
+
+//String
+const text = 'some text'
+
+//Number
+const number = 45
+
+//Boolean
+let value1 = true
+let value2 = false
+
+//Null
+const result = null -//will return the type as object (bug in JS)
+
+console.log(typeof text)
+console.log(typeof 'Hello');
+console.log(typeof value2);
+console.log(typeof true);
+console.log(typeof result);
+console.log(typeof null);
+
+
+
